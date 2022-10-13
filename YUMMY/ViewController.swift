32ae414be571a8b410deec63844716df2a1054fa
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2022-10-13 19:29:27 +0300

    Initial Commit

---
+++ YUMMY/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YUMMY
+//
+//  Created by Marina on 13.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
